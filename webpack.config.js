a7ee1d0d4f782756eaad777ee8aafeaf6a23d48e
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-06-26 18:32:39 +0300

    add css, sass, postcss, browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development';
+let target = 'web';
+
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
+  target = 'browserslist'
 }
 
 module.exports = {
   mode,
   devtool: "source-map",
+  target,
 
   module: {
     rules: [
       {
-        test: /\.js/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.scss/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
     ]
   },
+
+  plugins: [new MiniCssExtractPlugin()],
 
   devServer: {
     static: {

78ab0377b892e21aa20c3adb9db8d70b43318bd5
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-06-27 20:27:19 +0300

    fix links, add firebase deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4G6oYT3GHGmM1NJQp8KepidoC2N7J58s",
+  authDomain: "natours-119f0.firebaseapp.com",
+  projectId: "natours-119f0",
+  storageBucket: "natours-119f0.appspot.com",
+  messagingSenderId: "342483324668",
+  appId: "1:342483324668:web:dad697ea08abfd82d79179"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
